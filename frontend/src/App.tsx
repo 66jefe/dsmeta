01ed15c495b3b025d1bd2971d33073a5ac4288a4
Jeferson Prestes <jeferson_prestes@outlook.com>
2022-11-17 21:00:52 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
       <Header/>
-      <NotificationButton />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
